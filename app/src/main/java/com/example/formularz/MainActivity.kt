170419744a191aed5d347e84923994e71853c66b
matipoklo <poklojr@gmail.com>
2022-03-23 08:38:39 +0100

    Zmiana podejscia i okno dialogowe

--- app/src/main/java/com/example/formularz/MainActivity.kt
+++ app/src/main/java/com/example/formularz/MainActivity.kt
@@ -13,28 +13,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val kobieta = findViewById<RadioButton>(R.id.radioButton)
+        val sport = findViewById<CheckBox>(R.id.checkBox)
+        val muzyka = findViewById<CheckBox>(R.id.checkBox3)
+        val gry = findViewById<CheckBox>(R.id.checkBox4)
+        /*val wiek = findViewById<EditText>(R.id.editTextNumber).toString().toInt()*/
+        val turystyka = findViewById<CheckBox>(R.id.checkBox2)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-                val alert = CustomAlert()
-                alert.showDialog(this,"Welcome to Warmodroid !!!")
+                val result = StringBuilder()
+                result.append("Dane:")
+
+                if (kobieta.isChecked) {
+                    result.append("\nKobieta")
+                }
+
+                if (sport.isChecked) {
+                    result.append("\nSport")
+
+                }
+                if (muzyka.isChecked) {
+                    result.append("\nMuzyka")
+                }
+                if (gry.isChecked) {
+                    result.append("\nGry")
+                }
+                if (turystyka.isChecked) {
+                    result.append("\nTurystyka")
+                }
+
+                Toast.makeText(applicationContext, result.toString(), Toast.LENGTH_SHORT).show()
+
+
             }
-        }
     }
-
-class CustomAlert {
-
-    fun showDialog(activity: Activity?, msg: String?) {
-        val dialog = Dialog(activity!!)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dialog_layout)
-        val text = dialog.findViewById<View>(R.id.text_dialog) as TextView
-        text.text = msg
-        val dialogButton: Button = dialog.findViewById<View>(R.id.btn_dialog) as Button
-        dialogButton.setOnClickListener {
-            dialog.dismiss()
-        }
-        dialog.show()
     }
-}
