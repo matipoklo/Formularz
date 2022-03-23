909ff9db4e566c6b54808bfbc953caaf706b7604
matipoklo <poklojr@gmail.com>
2022-03-23 07:42:25 +0100

    Dodano wstepnie kod okienka wyswietlajacego info

--- app/src/main/java/com/example/formularz/MainActivity.kt
+++ app/src/main/java/com/example/formularz/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.formularz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.Activity
+import android.app.Dialog
+import android.provider.MediaStore
+import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val alert = CustomAlert()
+            val kobieta = findViewById<RadioButton>(R.id.radioButton)
+            val mezczyzna = findViewById<RadioButton>(R.id.radioButton4)
+            alert.showDialog(this,)
+        }
+    }
+}
+class CustomAlert {
+
+    fun showDialog(activity: Activity?, msg: String?) {
+        val dialog = Dialog(activity!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_layout)
+        val text = dialog.findViewById<View>(R.id.text_dialog) as TextView
+        text.text = msg
+        val dialogButton: Button = dialog.findViewById<View>(R.id.btn_dialog) as Button
+        dialogButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
